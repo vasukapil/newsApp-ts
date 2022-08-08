2d094a58f96329f5775485dd7d516e7c81df10c9
Krishna Jha <79072993+krishna-jha-25@users.noreply.github.com>
2022-08-08 15:30:23 +0530

    Update Homepage.tsx

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,104 +1,81 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-import React,{useState,useEffect,} from 'react'
+import React, { useState, useEffect, } from 'react'
 import './Homepage.css'
 
 
 
-const Homepage:React.FC = () => {
+const Homepage: React.FC = () => {
 
-    let navigate = useNavigate();
+    const pageSize = 5;
+    const navigate = useNavigate();
 
-    let[data,setData]=useState<any[]>([]);
-    const [pageSize,setPageSize]=useState(5);
-    let [dummy,setDummy]=useState<any[]>([]);
+    const [data, setData] = useState<any[]>([]);
+    // const [pageSize, setPageSize] = useState(5);
     const [page, setPage] = useState<number>(1)
 
 
 
-    useEffect(()=>{
-
-
-        axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=710a8d822eb14b20b65ac4f821e9fa80&page=${page}&pageSize=${pageSize}`)
-        .then(
-        res=>{
-       setDummy(res.data.articles);
-       if(page>1)
-       {
-           setDummy([...dummy,...res.data.articles]);
-       }
-        }
-        
-        )
-        
-        
-
-    },[page])
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=710a8d822eb14b20b65ac4f821e9fa80&page=${page}&pageSize=${pageSize}`)
+                const response = result.data.articles
+                setData([...data, ...response]);
+            } catch (error) {
+                console.log("Error in homepage", error);
+            }
+        })()
+    }, [page])
 
     useEffect(() => {
         const onScroll = function () {
-           if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-             console.log("you're at the bottom of the page")
-             setPage(()=>page+1);
-           
-           }
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                setPage(page + 1);
+
+            }
         }
         window.addEventListener('scroll', onScroll)
         return () => window.removeEventListener('scroll', onScroll)
-     },)
+    })
 
-   
+    return (
 
-    // english = data.filter((arr)=>{
-
-    //     return arr.country==='us'
-
-
-    // })
-
-
-
-   
-
-   
-      console.log(dummy)
-      console.log(page)
-    //   console.log(page)
-    //   console.log(pageSize)
-    
-
-  return (
-    <div>
-      
-        {
-
-
-            dummy && 
-            
-          
-            dummy.map((news,idx)=>(
-
-
-                <div key={idx}>
-                    <div className="news__component">
-                        <span>{idx+1}</span>
-                         <h2 style={{display:'inline-block'}}>{news.title}</h2>
-                         <p onClick={()=>{navigate('/category')}}>Read More...</p>
+        <div className="new-wrapper">
+            {
+                data?.map((news, i) => {
+                    <div className="new-item">
+                        <h3>
+                            {news.title}
+                        </h3>
                     </div>
-                </div>
-                
-            )
-                
+                })
+            }
+        </div>
+        // <div>
+        //     {
+        //         dummy &&
+        //         dummy.map((news, idx) => (
+        //             <div key={idx} >
+        //                 <div className="news__component" >
+        //                     <span>{idx + 1}</span>
+        //                     < h2 style={{ display: 'inline-block' }}> {news.title} < /h2>
+        //                         < p onClick={() => { navigate('/category') }}> Read More...</p>
+        //                         < /div>
+        //                         < /div>
 
-            )
+        //                         )
+
+        //                         )
 
 
            
             
-        }
-       
-    </div>
-  )
+        // }
+
+        //                 </div>
+        //                 )
+    )
 }
 
-export default Homepage
+export default Homepage;
